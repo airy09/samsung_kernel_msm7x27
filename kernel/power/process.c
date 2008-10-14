90c3640efecf16f08862106c6b04f9610b589889
Arve Hjønnevåg <arve@android.com>
2008-10-14 16:02:39 -0700

    PM: wakelock: Abort task freezing if a wake lock is held.
    Avoids a problem where the device sometimes hangs for 20 seconds
    before the screen is turned on.

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -15,16 +15,14 @@
 #include <linux/syscalls.h>
 #include <linux/freezer.h>
 #include <linux/delay.h>
-#include <linux/workqueue.h>
 #include <linux/wakelock.h>
-#include "power.h"
 
 /* 
  * Timeout for stopping processes
  */
 #define TIMEOUT	(20 * HZ)
 
-static inline int freezable(struct task_struct * p)
+static inline int freezeable(struct task_struct * p)
 {
 	if ((p == current) ||
 	    (p->flags & PF_NOFREEZE) ||
@@ -38,24 +36,19 @@ static int try_to_freeze_tasks(bool sig_only)
 	struct task_struct *g, *p;
 	unsigned long end_time;
 	unsigned int todo;
-	bool wq_busy = false;
 	struct timeval start, end;
 	u64 elapsed_csecs64;
 	unsigned int elapsed_csecs;
-	bool wakeup = false;
+	unsigned int wakeup = 0;
 
 	do_gettimeofday(&start);
 
 	end_time = jiffies + TIMEOUT;
-
-	if (!sig_only)
-		freeze_workqueues_begin();
-
 	while (true) {
 		todo = 0;
 		read_lock(&tasklist_lock);
 		do_each_thread(g, p) {
-			if (frozen(p) || !freezable(p))
+			if (frozen(p) || !freezeable(p))
 				continue;
 
 			if (!freeze_task(p, sig_only))
@@ -66,35 +59,18 @@ static int try_to_freeze_tasks(bool sig_only)
 			 * perturb a task in TASK_STOPPED or TASK_TRACED.
 			 * It is "frozen enough".  If the task does wake
 			 * up, it will immediately call try_to_freeze.
-			 *
-			 * Because freeze_task() goes through p's
-			 * scheduler lock after setting TIF_FREEZE, it's
-			 * guaranteed that either we see TASK_RUNNING or
-			 * try_to_stop() after schedule() in ptrace/signal
-			 * stop sees TIF_FREEZE.
 			 */
 			if (!task_is_stopped_or_traced(p) &&
 			    !freezer_should_skip(p))
 				todo++;
 		} while_each_thread(g, p);
 		read_unlock(&tasklist_lock);
-
-		if (!sig_only) {
-			wq_busy = freeze_workqueues_busy();
-			todo += wq_busy;
-		}
-
 		if (todo && has_wake_lock(WAKE_LOCK_SUSPEND)) {
 			wakeup = 1;
 			break;
 		}
 		if (!todo || time_after(jiffies, end_time))
 			break;
-
-		if (pm_wakeup_pending()) {
-			wakeup = true;
-			break;
-		}
 
 		/*
 		 * We need to retry, but first give the freezing tasks some
@@ -122,17 +98,13 @@ static int try_to_freeze_tasks(bool sig_only)
 		else {
 			printk("\n");
 			printk(KERN_ERR "Freezing of tasks failed after %d.%02d seconds "
-			       "(%d tasks refusing to freeze, wq_busy=%d):\n",
-			       elapsed_csecs / 100, elapsed_csecs % 100,
-			       todo - wq_busy, wq_busy);
+					"(%d tasks refusing to freeze):\n",
+					elapsed_csecs / 100, elapsed_csecs % 100, todo);
 		}
-		thaw_workqueues();
-
 		read_lock(&tasklist_lock);
 		do_each_thread(g, p) {
 			task_lock(p);
-			if (freezing(p) && !freezer_should_skip(p) &&
-				elapsed_csecs > 100)
+			if (freezing(p) && !freezer_should_skip(p))
 				sched_show_task(p);
 			cancel_freezing(p);
 			task_unlock(p);
@@ -159,10 +131,6 @@ int freeze_processes(void)
 		goto Exit;
 	printk("done.\n");
 
-	error = suspend_sys_sync_wait();
-	if (error)
-		goto Exit;
-
 	printk("Freezing remaining freezable tasks ... ");
 	error = try_to_freeze_tasks(false);
 	if (error)
@@ -183,7 +151,7 @@ static void thaw_tasks(bool nosig_only)
 
 	read_lock(&tasklist_lock);
 	do_each_thread(g, p) {
-		if (!freezable(p))
+		if (!freezeable(p))
 			continue;
 
 		if (nosig_only && should_send_signal(p))
@@ -202,7 +170,6 @@ void thaw_processes(void)
 	oom_killer_enable();
 
 	printk("Restarting tasks ... ");
-	thaw_workqueues();
 	thaw_tasks(true);
 	thaw_tasks(false);
 	schedule();
