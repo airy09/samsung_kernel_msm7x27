181d0a112ac8bda6814a7117bd772d52297a1253
Brian Swetland <swetland@google.com>
2010-02-24 16:13:20 -0800

    qsd8k: debugging / ramdump driver for QDSP6
    Provide a mechanism for userspace to get notification of DSP crashes,
    and to extract the DSP's RAM image before allowing the crash to "complete".
    
    Signed-off-by: Brian Swetland <swetland@google.com>

--- arch/arm/mach-msm/qdsp6/dsp_debug.c
+++ arch/arm/mach-msm/qdsp6/dsp_debug.c
@@ -22,33 +22,21 @@
 #include <linux/sched.h>
 #include <linux/wait.h>
 #include <linux/delay.h>
-#include <asm/atomic.h>
 
 #include "../proc_comm.h"
-#include <mach/debug_mm.h>
 
 static wait_queue_head_t dsp_wait;
 static int dsp_has_crashed;
 static int dsp_wait_count;
 
-static atomic_t dsp_crash_count = ATOMIC_INIT(0);
-
 void q6audio_dsp_not_responding(void)
 {
-
-	if (atomic_add_return(1, &dsp_crash_count) != 1) {
-		pr_err("q6audio_dsp_not_responding() - parking additional crasher...\n");
-		for (;;)
-			msleep(1000);
-	}
 	if (dsp_wait_count) {
 		dsp_has_crashed = 1;
 		wake_up(&dsp_wait);
 
 		while (dsp_has_crashed != 2)
 			wait_event(dsp_wait, dsp_has_crashed == 2);
-	} else {
-		pr_err("q6audio_dsp_not_responding() - no waiter?\n");
 	}
 	BUG();
 }
@@ -77,10 +65,9 @@ static ssize_t dsp_write(struct file *file, const char __user *buf,
 			int res;
 			dsp_wait_count++;
 			res = wait_event_interruptible(dsp_wait, dsp_has_crashed);
-			if (res < 0) {
-				dsp_wait_count--;
+			dsp_wait_count--;
+			if (res < 0)
 				return res;
-			}
 		}
 #if defined(CONFIG_MACH_MAHIMAHI)
 		/* assert DSP NMI */
@@ -93,8 +80,7 @@ static ssize_t dsp_write(struct file *file, const char __user *buf,
 		dsp_has_crashed = 2;
 		wake_up(&dsp_wait);
 	} else {
-		pr_err("[%s:%s] unknown dsp_debug command: %s\n", __MM_FILE__,
-				__func__, cmd);
+		pr_err("unknown dsp_debug command: %s\n", cmd);
 	}
 
 	return count;
@@ -128,14 +114,12 @@ static ssize_t dsp_read(struct file *file, char __user *buf,
 	while (count >= PAGE_SIZE) {
 		ptr = ioremap(addr, mapsize);
 		if (!ptr) {
-			pr_err("[%s:%s] map error @ %x\n", __MM_FILE__,
-					__func__, addr);
+			pr_err("dsp: map error @ %x\n", addr);
 			return -EFAULT;
 		}
 		if (copy_to_user(buf, ptr, PAGE_SIZE)) {
 			iounmap(ptr);
-			pr_err("[%s:%s] copy error @ %p\n", __MM_FILE__,
-					__func__, buf);
+			pr_err("dsp: copy error @ %p\n", buf);
 			return -EFAULT;
 		}
 		copy_ok_count += PAGE_SIZE;
