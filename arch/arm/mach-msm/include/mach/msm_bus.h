bba864e5eac7330feee6f55203a499ba812ca89b
Gagan Mac <gmac@codeaurora.org>
2010-09-13 13:04:18 -0600

    msm: Bus scaling driver
    This driver provides the ability to configure MSM bus performance across
    entire chipset. Clients can use MSM scaling APIs to request bandwidth
    and clock requirements for multiple master-slave pairs. The bus driver
    aggregates these requests for all the master-slave pairs on that path,
    and programs the hardware accordingly.
    
    Change-Id: Ib95eaba10c25ed1ce5e3bc55e40c06e57d6bffe8
    Signed-off-by: Gagan Mac <gmac@codeaurora.org>

--- arch/arm/mach-msm/include/mach/msm_bus.h
+++ arch/arm/mach-msm/include/mach/msm_bus.h
@@ -1,13 +1,34 @@
-/* Copyright (c) 2010-2011, Code Aurora Forum. All rights reserved.
+/* Copyright (c) 2010, Code Aurora Forum. All rights reserved.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 and
- * only version 2 as published by the Free Software Foundation.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, and the entire permission notice in its entirety,
+ *    including the disclaimer of warranties.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote
+ *    products derived from this software without specific prior
+ *    written permission.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * ALTERNATIVELY, this product may be distributed under the terms of
+ * the GNU General Public License, version 2, in which case the provisions
+ * of the GPL version 2 are required INSTEAD OF the BSD license.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, ALL OF
+ * WHICH ARE HEREBY DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+ * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF NOT ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
  */
 
 #ifndef _ARCH_ARM_MACH_MSM_BUS_H
@@ -25,26 +46,29 @@
  * R  : Ratio of actual bandwidth used to Tb
  * Ib : Instantaneous bandwidth
  * Ab : Arbitrated bandwidth
- *
- * IB_RECURRBLOCK and AB_RECURRBLOCK:
- * These are used if the requirement is to transfer a
- * recurring block of data over a known time window.
- *
- * IB_THROUGHPUTBW and AB_THROUGHPUTBW:
- * These are used for CPU style masters. Here the requirement
- * is to have minimum throughput bandwidth available to avoid
- * stalling.
  */
 #define IB_RECURRBLOCK(Ws, Bs) ((Ws) == 0 ? 0 : ((Bs)/(Ws)))
 #define AB_RECURRBLOCK(Ws, Per) ((Ws) == 0 ? 0 : ((Bs)/(Per)))
 #define IB_THROUGHPUTBW(Tb) (Tb)
 #define AB_THROUGHPUTBW(Tb, R) ((Tb) * (R))
 
+struct msm_bus_node_info {
+	unsigned int id;
+	int gateway;
+	int masterp;
+	int slavep;
+	int tier;
+	int ahb;
+	const char *slaveclk;
+	const char *memclk;
+	unsigned int buswidth;
+};
+
 struct msm_bus_vectors {
-	int src; /* Master */
-	int dst; /* Slave */
-	unsigned int ab; /* Arbitrated bandwidth */
-	unsigned int ib; /* Instantaneous bandwidth */
+	int src;
+	int dst;
+	int ab;
+	int ib;
 };
 
 struct msm_bus_paths {
@@ -55,59 +79,15 @@ struct msm_bus_paths {
 struct msm_bus_scale_pdata {
 	struct msm_bus_paths *usecase;
 	int num_usecases;
-	const char *name;
-	/*
-	 * If the active_only flag is set to 1, the BW request is applied
-	 * only when at least one CPU is active (powered on). If the flag
-	 * is set to 0, then the BW request is always applied irrespective
-	 * of the CPU state.
-	 */
-	unsigned int active_only;
 };
 
 /* Scaling APIs */
-
-/*
- * This function returns a handle to the client. This should be used to
- * call msm_bus_scale_client_update_request.
- * The function returns 0 if bus driver is unable to register a client
- */
-
-#ifdef CONFIG_MSM_BUS_SCALING
 uint32_t msm_bus_scale_register_client(struct msm_bus_scale_pdata *pdata);
 int msm_bus_scale_client_update_request(uint32_t cl, unsigned int index);
 void msm_bus_scale_unregister_client(uint32_t cl);
+
 /* AXI Port configuration APIs */
 int msm_bus_axi_porthalt(int master_port);
 int msm_bus_axi_portunhalt(int master_port);
-
-#else
-static inline uint32_t
-msm_bus_scale_register_client(struct msm_bus_scale_pdata *pdata)
-{
-	return 1;
-}
-
-static inline int
-msm_bus_scale_client_update_request(uint32_t cl, unsigned int index)
-{
-	return 0;
-}
-
-static inline void
-msm_bus_scale_unregister_client(uint32_t cl)
-{
-}
-
-static inline int msm_bus_axi_porthalt(int master_port)
-{
-	return 0;
-}
-
-static inline int msm_bus_axi_portunhalt(int master_port)
-{
-	return 0;
-}
-#endif
 
 #endif /*_ARCH_ARM_MACH_MSM_BUS_H*/
