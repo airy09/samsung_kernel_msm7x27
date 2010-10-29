8b4e5ab4770433c7e8e058c1aee0a84f2c667c07
Maheshkumar Sivasubramanian <msivasub@codeaurora.org>
2010-10-29 17:31:01 -0600

    msm: 8x60: remove unused power collapse mode.
    Remove references to MSM_PM_SLEEP_MODE_POWER_COLLAPSE_SHALLOW_VDD_MIN
    
    Change-Id: I1994a6c550ab807e678a772635e881e49c645da7
    Signed-off-by: Maheshkumar Sivasubramanian <msivasub@codeaurora.org>

--- arch/arm/mach-msm/pm.h
+++ arch/arm/mach-msm/pm.h
@@ -1,6 +1,7 @@
 /* arch/arm/mach-msm/pm.h
  *
  * Copyright (C) 2007 Google, Inc.
+ * Copyright (c) 2009-2010, Code Aurora Forum. All rights reserved.
  * Author: San Mehat <san@android.com>
  *
  * This software is licensed under the terms of the GNU General Public
@@ -17,15 +18,45 @@
 #ifndef __ARCH_ARM_MACH_MSM_PM_H
 #define __ARCH_ARM_MACH_MSM_PM_H
 
-#include <asm/arch/msm_iomap.h>
+#include <linux/types.h>
+#include <linux/cpuidle.h>
 
-#define A11S_CLK_SLEEP_EN_ADDR MSM_CSR_BASE + 0x11c
-
-#define CLK_SLEEP_EN_ARM11_CORE	0x01
-#define CLK_SLEEP_EN_ARM11_AHB	0x02
-#define CLK_SLEEP_EN_ID_BRIDGE	0x04
-#define CLK_SLEEP_EN_DMA_BRIDGE	0x08
-#define CLK_SLEEP_EN_PBUS	0x10
-#define CLK_SLEEP_EN_DEBUG_TIME	0x20
-#define CLK_SLEEP_EN_GP_TIMER	0x40
+#ifdef CONFIG_HOTPLUG_CPU
+extern int pen_release;
+extern void msm_secondary_startup(void);
+#else
+#define msm_secondary_startup NULL
 #endif
+
+enum msm_pm_sleep_mode {
+	MSM_PM_SLEEP_MODE_POWER_COLLAPSE_SUSPEND,
+	MSM_PM_SLEEP_MODE_POWER_COLLAPSE,
+	MSM_PM_SLEEP_MODE_APPS_SLEEP,
+	MSM_PM_SLEEP_MODE_RAMP_DOWN_AND_WAIT_FOR_INTERRUPT,
+	MSM_PM_SLEEP_MODE_WAIT_FOR_INTERRUPT,
+	MSM_PM_SLEEP_MODE_POWER_COLLAPSE_NO_XO_SHUTDOWN,
+	MSM_PM_SLEEP_MODE_POWER_COLLAPSE_STANDALONE,
+	MSM_PM_SLEEP_MODE_NR
+};
+
+#define MSM_PM_MODE(cpu, mode_nr)  ((cpu) * MSM_PM_SLEEP_MODE_NR + (mode_nr))
+
+struct msm_pm_platform_data {
+	u8 supported;
+	u8 suspend_enabled;  /* enabled for suspend */
+	u8 idle_enabled;     /* enabled for idle low power */
+	u32 latency;         /* interrupt latency in microseconds when entering
+				and exiting the low power mode */
+	u32 residency;       /* time threshold in microseconds beyond which
+				staying in the low power mode saves power */
+};
+
+void msm_pm_set_platform_data(struct msm_pm_platform_data *data, int count);
+int msm_pm_idle_prepare(struct cpuidle_device *dev);
+int msm_pm_idle_enter(enum msm_pm_sleep_mode sleep_mode);
+
+#ifdef CONFIG_HOTPLUG_CPU
+int msm_pm_platform_secondary_init(unsigned int cpu);
+#endif
+
+#endif  /* __ARCH_ARM_MACH_MSM_PM_H */
