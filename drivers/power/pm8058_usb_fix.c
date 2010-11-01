65e94bd10ba12a9b59a7634961bdceb9f7dda1b4
Abhijeet Dharmapurikar <adharmap@codeaurora.org>
2010-11-01 15:49:19 -0700

    pm8058_usb_fix: implement workaround for stuck VCHG
    Some boards do not use the charging feature of the pmic8058 chip. They
    however need the software workaround for an issue where the VCHG remains
    high even when the USB cable it removed.
    
    Change-Id: Ic92ac0cd913ce88b86706ea013d17789f1acf791
    Signed-off-by: Abhijeet Dharmapurikar <adharmap@codeaurora.org>

--- drivers/power/pm8058_usb_fix.c
+++ drivers/power/pm8058_usb_fix.c
@@ -9,6 +9,11 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ *
  */
 
 #include <linux/module.h>
@@ -300,7 +305,7 @@ static int __devinit pm8058_charger_probe(struct platform_device *pdev)
 {
 	struct pm8058_chip *pm_chip;
 
-	pm_chip = dev_get_drvdata(pdev->dev.parent);
+	pm_chip = platform_get_drvdata(pdev);
 	if (pm_chip == NULL) {
 		pr_err("%s:no parent data passed in.\n", __func__);
 		return -EFAULT;
