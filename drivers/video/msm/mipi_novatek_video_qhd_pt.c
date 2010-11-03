5ae8ef7d4d384c3d21df3f3331c618aaa6c3e39f
kuogee hsieh <khsieh@codeaurora.org>
2010-11-03 09:19:57 -0700

    msm_fb: display Add Novatek video/cmd panel support
    Incorporated both video/command mode panel configuation
    to mipi_host_init to support both video/command.
    
    Change-Id: I51555e9065038607765de54e98c36128aa611d8f
    Signed-off-by: Kuogee Hsieh <khsieh@codeaurora.org>

--- drivers/video/msm/mipi_novatek_video_qhd_pt.c
+++ drivers/video/msm/mipi_novatek_video_qhd_pt.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2010-2011, Code Aurora Forum. All rights reserved.
+/* Copyright (c) 2010, Code Aurora Forum. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -9,6 +9,11 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ *
  */
 
 #include "msm_fb.h"
@@ -18,20 +23,16 @@
 static struct msm_panel_info pinfo;
 
 static struct mipi_dsi_phy_ctrl dsi_video_mode_phy_db = {
-/* DSI_BIT_CLK at 500MHz, 2 lane, RGB888 */
+/* DSI_BIT_CLK at 400MHz, 1 lane, RGB888 */
 		{0x03, 0x01, 0x01, 0x00},	/* regulator */
 		/* timing   */
-		{0x82, 0x31, 0x13, 0x0, 0x42, 0x4D, 0x18,
-		0x35, 0x21, 0x03, 0x04},
+		{0x96, 0x26, 0x23, 0x0, 0x50, 0x4B, 0x1e,
+		0x28, 0x28, 0x03, 0x04},
 		{0x7f, 0x00, 0x00, 0x00},	/* phy ctrl */
 		{0xee, 0x02, 0x86, 0x00},	/* strength */
 		/* pll control */
 		{0x40, 0xf9, 0xb0, 0xda, 0x00, 0x50, 0x48, 0x63,
-#if defined(NOVATEK_TWO_LANE)
-		0x30, 0x07, 0x03,
-#else           /* default set to 1 lane */
 		0x30, 0x07, 0x07,
-#endif
 		0x05, 0x14, 0x03, 0x0, 0x0, 0x54, 0x06, 0x10, 0x04, 0x0},
 };
 
@@ -39,8 +40,10 @@ static int __init mipi_video_novatek_qhd_pt_init(void)
 {
 	int ret;
 
+#ifdef CONFIG_FB_MSM_MIPI_PANEL_DETECT
 	if (msm_fb_detect_client("mipi_video_novatek_qhd"))
 		return 0;
+#endif
 
 	pinfo.xres = 540;
 	pinfo.yres = 960;
@@ -48,24 +51,25 @@ static int __init mipi_video_novatek_qhd_pt_init(void)
 	pinfo.pdest = DISPLAY_1;
 	pinfo.wait_cycle = 0;
 	pinfo.bpp = 24;
-	pinfo.lcdc.h_back_porch = 80;
-	pinfo.lcdc.h_front_porch = 24;
-	pinfo.lcdc.h_pulse_width = 8;
-	pinfo.lcdc.v_back_porch = 16;
-	pinfo.lcdc.v_front_porch = 8;
-	pinfo.lcdc.v_pulse_width = 1;
+	pinfo.lcdc.h_back_porch = 50;
+	pinfo.lcdc.h_front_porch = 50;
+	pinfo.lcdc.h_pulse_width = 20;
+	pinfo.lcdc.v_back_porch = 11;
+	pinfo.lcdc.v_front_porch = 10;
+	pinfo.lcdc.v_pulse_width = 5;
 	pinfo.lcdc.border_clr = 0;	/* blk */
 	pinfo.lcdc.underflow_clr = 0xff;	/* blue */
 	pinfo.lcdc.hsync_skew = 0;
 	pinfo.bl_max = 15;
 	pinfo.bl_min = 1;
 	pinfo.fb_num = 2;
+	pinfo.clk_rate = 500000000;
 
 	pinfo.mipi.mode = DSI_VIDEO_MODE;
 	pinfo.mipi.pulse_mode_hsa_he = TRUE;
-	pinfo.mipi.hfp_power_stop = FALSE;
-	pinfo.mipi.hbp_power_stop = FALSE;
-	pinfo.mipi.hsa_power_stop = FALSE;
+	pinfo.mipi.hfp_power_stop = TRUE;
+	pinfo.mipi.hbp_power_stop = TRUE;
+	pinfo.mipi.hsa_power_stop = TRUE;
 	pinfo.mipi.eof_bllp_power_stop = TRUE;
 	pinfo.mipi.bllp_power_stop = TRUE;
 	pinfo.mipi.traffic_mode = DSI_NON_BURST_SYNCH_PULSE;
@@ -73,21 +77,16 @@ static int __init mipi_video_novatek_qhd_pt_init(void)
 	pinfo.mipi.vc = 0;
 	pinfo.mipi.rgb_swap = DSI_RGB_SWAP_BGR;
 	pinfo.mipi.data_lane0 = TRUE;
-	pinfo.mipi.esc_byte_ratio = 4;
-#if defined(NOVATEK_TWO_LANE)
-	pinfo.mipi.data_lane1 = TRUE;
-#endif
 	pinfo.mipi.tx_eot_append = TRUE;
-	pinfo.mipi.t_clk_post = 0x04;
-	pinfo.mipi.t_clk_pre = 0x1c;
+	pinfo.mipi.t_clk_post = 10;
+	pinfo.mipi.t_clk_pre = 30;
 	pinfo.mipi.stream = 0; /* dma_p */
 	pinfo.mipi.mdp_trigger = DSI_CMD_TRIGGER_SW;
 	pinfo.mipi.dma_trigger = DSI_CMD_TRIGGER_SW;
-	pinfo.mipi.frame_rate = 60;
 	pinfo.mipi.dsi_phy_db = &dsi_video_mode_phy_db;
 
 	ret = mipi_novatek_device_register(&pinfo, MIPI_DSI_PRIM,
-						MIPI_DSI_PANEL_QHD_PT);
+						MIPI_DSI_PANEL_WVGA_PT);
 	if (ret)
 		pr_err("%s: failed to register device!\n", __func__);
 
