moetayuko/CAF_kernel_msm-3.18

msm: clock: Migrate to clkdev
Migrating to clkdev has several advantages:

* Less code in mach-msm/clock.c

* A more robust clk_get() implementation

* clk_add_alias() support

* clk_get_sys() support

In general, this will help board authors setup clock aliases and
break the dependency on device pointers in the clock tables.

Change-Id: Ie6a49c1f24a35d22d4d308c493ac6927e7d5b348
Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>

arch/arm/mach-msm/clock-7x30.h
@@ -147,21 +147,25 @@ void pll_disable(uint32_t pll);
 extern int internal_pwr_rail_ctl_auto(unsigned rail_id, bool enable);
 
 #define CLK_7X30(clk_name, clk_id, clk_dev, clk_flags) {	\
-	.name = clk_name, \
-	.id = L_7X30_##clk_id, \
-	.remote_id = P_##clk_id, \
-	.flags = clk_flags, \
-	.dev = clk_dev, \
-	.dbg_name = #clk_id, \
+	.con_id = clk_name, \
+	.dev_id = clk_dev, \
+	.clk = &(struct clk){ \
+		.id = L_7X30_##clk_id, \
+		.remote_id = P_##clk_id, \
+		.flags = clk_flags, \
+		.dbg_name = #clk_id, \
+	}, \
 	}
 
 #define CLK_7X30S(clk_name, l_id, r_id, clk_dev, clk_flags) {	\
-	.name = clk_name, \
-	.id = L_7X30_##l_id, \
-	.remote_id = P_##r_id, \
-	.flags = clk_flags, \
-	.dev = clk_dev, \
-	.dbg_name = #l_id, \
+	.con_id = clk_name, \
+	.dev_id = clk_dev, \
+	.clk = &(struct clk){ \
+		.id = L_7X30_##l_id, \
+		.remote_id = P_##r_id, \
+		.flags = clk_flags, \
+		.dbg_name = #l_id, \
+	}, \
 	}
 
 #endif

arch/arm/mach-msm/clock-pcom.h
@@ -147,13 +147,15 @@ enum clk_reset_action;
 int pc_clk_reset(unsigned id, enum clk_reset_action action);
 
 #define CLK_PCOM(clk_name, clk_id, clk_dev, clk_flags) {	\
-	.name = clk_name, \
-	.id = P_##clk_id, \
-	.remote_id = P_##clk_id, \
-	.ops = &clk_ops_pcom, \
-	.flags = clk_flags, \
-	.dev = clk_dev, \
-	.dbg_name = #clk_id, \
+	.con_id = clk_name, \
+	.dev_id = clk_dev, \
+	.clk = &(struct clk){ \
+		.id = P_##clk_id, \
+		.remote_id = P_##clk_id, \
+		.ops = &clk_ops_pcom, \
+		.flags = clk_flags, \
+		.dbg_name = #clk_id, \
+	}, \
 	}
 
 #endif

arch/arm/mach-msm/clock.c
@@ -22,7 +22,6 @@
 #include <linux/err.h>
 #include <linux/clk.h>
 #include <linux/slab.h>
-#include <linux/spinlock.h>
 #include <linux/debugfs.h>
 #include <linux/ctype.h>
 #include <linux/pm_qos_params.h>
@@ -30,46 +29,15 @@
 #include <linux/seq_file.h>
 #include <mach/clk.h>
 
+#include <asm/clkdev.h>
+
 #include "clock.h"
 #include "proc_comm.h"
 #include "clock-7x30.h"
 #include "clock-pcom.h"
 
 static DEFINE_MUTEX(clocks_mutex);
-static DEFINE_SPINLOCK(clocks_lock);
 static HLIST_HEAD(clocks);
-struct clk *msm_clocks;
-unsigned msm_num_clocks;
-
-/*
- * Standard clock functions defined in include/linux/clk.h
- */
-struct clk *clk_get(struct device *dev, const char *id)
-{
-	struct clk *clk;
-	struct hlist_node *pos;
-
-	mutex_lock(&clocks_mutex);
-
-	hlist_for_each_entry(clk, pos, &clocks, list)
-		if (!strcmp(id, clk->name) && clk->dev == dev)
-			goto found_it;
-
-	hlist_for_each_entry(clk, pos, &clocks, list)
-		if (!strcmp(id, clk->name) && clk->dev == NULL)
-			goto found_it;
-
-	clk = ERR_PTR(-ENOENT);
-found_it:
-	mutex_unlock(&clocks_mutex);
-	return clk;
-}
-EXPORT_SYMBOL(clk_get);
-
-void clk_put(struct clk *clk)
-{
-}
-EXPORT_SYMBOL(clk_put);
 
 int clk_enable(struct clk *clk)
 {
@@ -174,22 +142,22 @@ static void __init set_clock_ops(struct clk *clk)
 	}
 }
 
-void __init msm_clock_init(struct clk *clock_tbl, unsigned num_clocks)
+void __init msm_clock_init(struct clk_lookup *clock_tbl, unsigned num_clocks)
 {
 	unsigned n;
 	struct clk *clk;
 
 	mutex_lock(&clocks_mutex);
-	msm_clocks = clock_tbl;
-	msm_num_clocks = num_clocks;
-	for (n = 0; n < msm_num_clocks; n++) {
-		set_clock_ops(&msm_clocks[n]);
-		hlist_add_head(&msm_clocks[n].list, &clocks);
+
+	for (n = 0; n < num_clocks; n++) {
+		set_clock_ops(clock_tbl[n].clk);
+		clkdev_add(&clock_tbl[n]);
+		hlist_add_head(&clock_tbl[n].clk->list, &clocks);
 	}
 	mutex_unlock(&clocks_mutex);
 
-	for (n = 0; n < msm_num_clocks; n++) {
-		clk = &msm_clocks[n];
+	for (n = 0; n < num_clocks; n++) {
+		clk = clock_tbl[n].clk;
 		if (clk->flags & CLKFLAG_VOTER) {
 			struct clk *agg_clk = clk_get(NULL, clk->aggregator);
 			BUG_ON(IS_ERR(agg_clk));
@@ -287,8 +255,6 @@ static int clk_info_seq_show(struct seq_file *seq, void *v)
 	seq_printf(seq, "Clock %s\n", clk->dbg_name);
 	seq_printf(seq, "  Id          %d\n", clk->id);
 	seq_printf(seq, "  Flags       %x\n", clk->flags);
-	seq_printf(seq, "  Dev         %p %s\n",
-			clk->dev, clk->dev ? dev_name(clk->dev) : "");
 	seq_printf(seq, "  Enabled     %d\n", clk->ops->is_enabled(clk->id));
 	seq_printf(seq, "  Rate        %ld\n", clk_get_rate(clk));
 

airy09/samsung_kernel_msm7x27

 * Less code in mach-msm/clock.c
 * A more robust clk_get() implementation
 * clk_add_alias() support
 * clk_get_sys() support
Change-Id: I51f055ef89f279fac39b63e74e1f552f5bdec975

arch/arm/mach-msm/clock-7x30.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2009-2010, Code Aurora Forum. All rights reserved.
+/* Copyright (c) 2009-2011, Code Aurora Forum. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -153,31 +153,38 @@ enum clk_sources {
 extern int internal_pwr_rail_ctl_auto(unsigned rail_id, bool enable);
 
 extern struct clk_ops soc_clk_ops_7x30;
+
 #define CLK_7X30(clk_name, clk_id, clk_dev, clk_flags) {	\
-	.name = clk_name, \
-	.id = L_##clk_id, \
-	.remote_id = P_##clk_id, \
-	.flags = clk_flags, \
-	.dev = clk_dev, \
-	.dbg_name = #clk_id, \
+	.con_id = clk_name, \
+	.dev_id = clk_dev, \
+	.clk = &(struct clk){ \
+		.id = L_##clk_id, \
+		.remote_id = P_##clk_id, \
+		.flags = clk_flags, \
+		.dbg_name = #clk_id, \
+	}, \
 	}
 
 #define CLK_7X30S(clk_name, l_id, r_id, clk_dev, clk_flags) {	\
-	.name = clk_name, \
-	.id = L_##l_id, \
-	.remote_id = P_##r_id, \
-	.flags = clk_flags, \
-	.dev = clk_dev, \
-	.dbg_name = #l_id, \
+	.con_id = clk_name, \
+	.dev_id = clk_dev, \
+	.clk = &(struct clk){ \
+		.id = L_##l_id, \
+		.remote_id = P_##r_id, \
+		.flags = clk_flags, \
+		.dbg_name = #l_id, \
+	}, \
 	}
 
 #define CLK_7X30L(clk_name, l_id, clk_dev, clk_flags) {	\
-	.name = clk_name, \
-	.id = L_##l_id, \
-	.flags = clk_flags, \
-	.dev = clk_dev, \
-	.dbg_name = #l_id, \
-	.ops = &soc_clk_ops_7x30, \
+	.con_id = clk_name, \
+	.dev_id = clk_dev, \
+	.clk = &(struct clk){ \
+		.id = L_##l_id, \
+		.flags = clk_flags, \
+		.dbg_name = #l_id, \
+		.ops = &soc_clk_ops_7x30, \
+	}, \
 	}
 
 #endif

arch/arm/mach-msm/clock-pcom.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2009, Code Aurora Forum. All rights reserved.
+/* Copyright (c) 2009-2011, Code Aurora Forum. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -144,13 +144,15 @@ extern struct clk_ops clk_ops_pcom_div2;
 int pc_clk_reset(unsigned id, enum clk_reset_action action);
 
 #define CLK_PCOM(clk_name, clk_id, clk_dev, clk_flags) {	\
-	.name = clk_name, \
-	.id = P_##clk_id, \
-	.remote_id = P_##clk_id, \
-	.ops = &clk_ops_remote, \
-	.flags = clk_flags, \
-	.dev = clk_dev, \
-	.dbg_name = #clk_id, \
+	.con_id = clk_name, \
+	.dev_id = clk_dev, \
+	.clk = &(struct clk){ \
+		.id = P_##clk_id, \
+		.remote_id = P_##clk_id, \
+		.ops = &clk_ops_remote, \
+		.flags = clk_flags, \
+		.dbg_name = #clk_id, \
+	}, \
 	}
 
 #endif

arch/arm/mach-msm/clock.c
@@ -1,7 +1,7 @@
 /* arch/arm/mach-msm/clock.c
  *
  * Copyright (C) 2007 Google, Inc.
- * Copyright (c) 2007-2010, Code Aurora Forum. All rights reserved.
+ * Copyright (c) 2007-2011, Code Aurora Forum. All rights reserved.
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -19,6 +19,10 @@
 #include <linux/err.h>
 #include <linux/spinlock.h>
 #include <linux/pm_qos_params.h>
+#include <linux/clk.h>
+
+#include <asm/clkdev.h>
+
 #include <mach/socinfo.h>
 
 #include "clock.h"
@@ -32,32 +36,6 @@ static struct notifier_block axi_freq_notifier_block;
 /*
  * Standard clock functions defined in include/linux/clk.h
  */
-struct clk *clk_get(struct device *dev, const char *id)
-{
-	struct clk *clk;
-
-	mutex_lock(&clocks_mutex);
-
-	list_for_each_entry(clk, &clocks, list)
-		if (!strcmp(id, clk->name) && clk->dev == dev)
-			goto found_it;
-
-	list_for_each_entry(clk, &clocks, list)
-		if (!strcmp(id, clk->name) && clk->dev == NULL)
-			goto found_it;
-
-	clk = ERR_PTR(-ENOENT);
-found_it:
-	mutex_unlock(&clocks_mutex);
-	return clk;
-}
-EXPORT_SYMBOL(clk_get);
-
-void clk_put(struct clk *clk)
-{
-}
-EXPORT_SYMBOL(clk_put);
-
 int clk_enable(struct clk *clk)
 {
 	return clk->ops->enable(clk->id);
@@ -204,7 +182,7 @@ static int axi_freq_notifier_handler(struct notifier_block *block,
 }
 
 
-void __init msm_clock_init(struct clk *clock_tbl, unsigned num_clocks)
+void __init msm_clock_init(struct clk_lookup *clock_tbl, unsigned num_clocks)
 {
 	unsigned n;
 	struct clk *clk;
@@ -214,13 +192,14 @@ void __init msm_clock_init(struct clk *clock_tbl, unsigned num_clocks)
 
 	mutex_lock(&clocks_mutex);
 	for (n = 0; n < num_clocks; n++) {
-		msm_clk_soc_set_ops(&clock_tbl[n]);
-		list_add_tail(&clock_tbl[n].list, &clocks);
+		msm_clk_soc_set_ops(clock_tbl[n].clk);
+		clkdev_add(&clock_tbl[n]);
+		list_add_tail(&clock_tbl[n].clk->list, &clocks);
 	}
 	mutex_unlock(&clocks_mutex);
 
 	for (n = 0; n < num_clocks; n++) {
-		clk = &clock_tbl[n];
+		clk = clock_tbl[n].clk;
 		if (clk->flags & CLKFLAG_VOTER) {
 			struct clk *agg_clk = clk_get(NULL, clk->aggregator);
 			BUG_ON(IS_ERR(agg_clk));